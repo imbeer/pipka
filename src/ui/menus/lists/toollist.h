78b586b28fd920083571a732a8dace4237cd0da0
imbeer <alekceykocenko@gmail.com>
2025-03-28 12:34:04 +0300

    Remove cpp file for template 'toollist'

--- src/ui/menus/lists/toollist.h
+++ src/ui/menus/lists/toollist.h
@@ -4,6 +4,10 @@
 #include <type_traits>
 #include <QListView>
 #include <QWidget>
+#include "layeritemdelegate.h"
+#include "layerlistmodel.h"
+
+#include <QVBoxLayout>
 #include "../../../control/controller.h"
 
 namespace PIPKA::UI {
@@ -35,6 +39,39 @@ protected:
     Delegate *m_delegate;
 
 };
+
+
+
+
+///
+template <typename Model, typename Delegate>
+ToolList<Model, Delegate>::ToolList(
+    std::shared_ptr<CONTROL::Controller> &controller,
+    const int &w, const int &h,
+    QWidget *parent)
+    : QWidget{parent}, m_controller(controller)
+{
+    m_listView = new QListView(this);
+    m_model = new Model(controller, this);
+    m_delegate = new Delegate(this);
+    setBaseSize(w, h);
+    initUi();
+}
+
+template <typename Model, typename Delegate>
+void ToolList<Model, Delegate>::initUi()
+{
+    m_listView->setModel(m_model);
+    m_listView->setItemDelegate(m_delegate);
+    m_listView->setStyleSheet("QListView { background: transparent; }");
+    m_listView->setContentsMargins(0, 0, 0, 0);
+    m_listView->setSpacing(0);
+
+    // Layout
+    auto *layout = new QVBoxLayout(this);
+    layout->addWidget(m_listView);
+    setLayout(layout);
+}
 
 }
 
